da82bbaabfb669704798497b2cd284be475ab94a
ShinobiKoda <adebiyipacsc2022@futa.edu.ng>
2025-07-22 23:05:01 +0100

    Implement code changes to enhance functionality and improve performance

--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -1,10 +1,71 @@
 "use client";
+import Image from "next/image";
 
 export function Homepage() {
   return (
-    <div>
-      Hello
-      <div>hello</div>
+    <div className="w-full py-8">
+      <section className="bg-[#1a8671]"></section>
+
+      <section>
+        <h1 className="text-xl lg:text-4xl text-center font-semibold">
+          Affiliate <span className=" text-[#1a8671]">Business</span> Marketing
+        </h1>
+        <div
+          className="w-full h-[500px] bg-center bg-cover mt-8"
+          style={{
+            backgroundImage: "url('/Images/business-meeting.jpg')",
+          }}
+        ></div>
+
+        <div className="mt-28 w-full px-4 lg:px-12 max-w-[1440px] mx-auto">
+          <div className="w-full flex-col space-y-2">
+            <h2 className="font-bold text-xl lg:text-3xl italic text-[#1a8671]">
+              Affiliate Sucess Strategies
+            </h2>
+            <div className="w-full h-1 bg-[#1a8671]"></div>
+          </div>
+
+          <div className="w-full max-w-[1440px] mx-auto mt-8 grid grid-cols-1 lg:grid-cols-2 gap-8">
+            <div className="w-full flex flex-col items-center justify-center">
+              <div className="max-w-[400px] max-h-[400px] w-full">
+                <Image
+                  src="/Images/great-content-writing.jpg"
+                  alt="Strategy Image"
+                  width={100}
+                  height={100}
+                  className="w-full"
+                />
+              </div>
+              <p className="text-white font-medium py-6 w-full bg-[#1a8671] px-3 text-base lg:text-xl max-w-[500px]">
+                CONTENT WRITING
+              </p>
+            </div>
+            <div className="w-full flex flex-col items-center justify-center">
+              <div className="max-w-[400px] max-h-[400px] w-full">
+                <Image
+                  src="/Images/youtube-promotion.jpg"
+                  alt="Strategy Image"
+                  width={100}
+                  height={100}
+                  className="w-full"
+                />
+              </div>
+              <p className="text-white font-medium py-6 w-full bg-[#1a8671] px-3 text-base lg:text-xl max-w-[500px]">
+                Youtube Promotion
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="w-full mt-[200px]  px-4 lg:px-12 max-w-[1440px] mx-auto">
+          <div className="w-full flex-col space-y-2">
+            <h2 className="font-bold text-xl lg:text-3xl italic text-[#1a8671]">
+              Customer Review and Testimonials
+            </h2>
+            <div className="w-full h-1 bg-[#1a8671]"></div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -3,6 +3,8 @@ import Link from "next/link";
 import Image from "next/image";
 import { useState } from "react";
 import { motion } from "motion/react";
+import { FaYoutube } from "react-icons/fa";
+import { FaPinterestP } from "react-icons/fa6";
 
 export function Navbar() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -10,136 +12,135 @@ export function Navbar() {
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
   };
+
   return (
-    <nav className="w-full fixed top-0 left-0 z-50">
-      <div className="w-full hidden lg:flex">
-        <Image
-          src="https://us-ms.gr-cdn.com/getresponse-tnbXo/photos/a36a85e4-f75d-40ad-a202-a0b8e5d08585.jpeg"
-          alt="Logo Image"
-          width={100}
-          height={100}
-        />
-        <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Donate</li>
-          <li>Contact</li>
-        </ul>
+    <nav className="fixed top-0 z-50 left-0 w-full">
+      <div className="w-full max-w-[1440px] mx-auto">
+        <div className="w-full hidden lg:flex lg:items-center lg:justify-between py-6 lg:px-12">
+          <div className="flex items-center gap-12">
+            <Image
+              src="https://us-ms.gr-cdn.com/getresponse-tnbXo/photos/a36a85e4-f75d-40ad-a202-a0b8e5d08585.jpeg"
+              alt="Logo Image"
+              width={50}
+              height={50}
+            />
+            <ul className="flex items-center gap-10 *:cursor-pointer *:hover:opacity-85">
+              <li>Home</li>
+              <li>About</li>
+              <li>Donate</li>
+              <li>Contact</li>
+            </ul>
+          </div>
 
-        <div>
-          <Link href="#">Youtube</Link>
-          <Link href="#">Facebook</Link>
-          <Link href="#">Pinterest</Link>
-          <Link href="#">ABM</Link>
-        </div>
-      </div>
+          <div className="flex items-center gap-8 *:cursor-pointer *:hover:opacity-85">
+            <Link href="https://www.youtube.com/@Djisrael27" target="_blank">
+              <FaYoutube size={35} />
+            </Link>
 
-      <div className="flex items-center justify-between px-4 lg:hidden py-4 w-full">
-        <div className="w-[43px] h-[43px]">
-          <Image
-            src="https://us-ms.gr-cdn.com/getresponse-tnbXo/photos/a36a85e4-f75d-40ad-a202-a0b8e5d08585.jpeg"
-            alt="Logo Image"
-            width={30}
-            height={30}
-            className="w-full"
-          />
-        </div>
-        <button
-          onClick={toggleMenu}
-          className="flex flex-col h-8 justify-between w-10 relative"
-          aria-label="Toggle menu"
-        >
-          <motion.div
-            className="h-1 bg-black w-full origin-center"
-            animate={isMenuOpen ? { rotate: 45, y: 8 } : { rotate: 0, y: 0 }}
-            transition={{ duration: 0.3, ease: "easeInOut" }}
-          />
-          <motion.div
-            className="h-1 bg-black w-full"
-            animate={isMenuOpen ? { opacity: 0 } : { opacity: 1 }}
-            transition={{ duration: 0.3, ease: "easeInOut" }}
-          />
-          <motion.div
-            className="h-1 bg-black w-full origin-center"
-            animate={isMenuOpen ? { rotate: -45, y: -19 } : { rotate: 0, y: 0 }}
-            transition={{ duration: 0.3, ease: "easeInOut" }}
-          />
-        </button>
-      </div>
-
-      <motion.div
-        className="lg:hidden bg-white border-t border-gray-200 overflow-hidden"
-        initial={false}
-        animate={
-          isMenuOpen
-            ? { height: "auto", opacity: 1 }
-            : { height: 0, opacity: 0 }
-        }
-        transition={{ duration: 0.3, ease: "easeInOut" }}
-      >
-        <div className="px-4 py-6 space-y-4">
-          <ul className="space-y-4">
-            <li>
-              <Link
-                href="/"
-                className="block text-lg hover:text-blue-600 transition-colors"
-              >
-                Home
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="/#"
-                className="block text-lg hover:text-blue-600 transition-colors"
-              >
-                About
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="/#"
-                className="block text-lg hover:text-blue-600 transition-colors"
-              >
-                Donate
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="/#"
-                className="block text-lg hover:text-blue-600 transition-colors"
-              >
-                Contact
-              </Link>
-            </li>
-          </ul>
-          <div className="pt-4 border-t border-gray-200 space-y-3">
             <Link
-              href="#"
-              className="block text-sm hover:text-blue-600 transition-colors"
+              href="https://www.pinterest.com/akinpelu_israel/"
+              target="_blank"
             >
-              Youtube
-            </Link>
-            <Link
-              href="#"
-              className="block text-sm hover:text-blue-600 transition-colors"
-            >
-              Facebook
-            </Link>
-            <Link
-              href="#"
-              className="block text-sm hover:text-blue-600 transition-colors"
-            >
-              Pinterest
-            </Link>
-            <Link
-              href="#"
-              className="block text-sm bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700 transition-colors text-center"
-            >
-              ABM
+              <FaPinterestP size={35} />
             </Link>
           </div>
         </div>
-      </motion.div>
+
+        <div className="flex items-center justify-between px-4 lg:hidden py-4 w-full border-b border-gray-200">
+          <div className="w-[43px] h-[43px]">
+            <Image
+              src="https://us-ms.gr-cdn.com/getresponse-tnbXo/photos/a36a85e4-f75d-40ad-a202-a0b8e5d08585.jpeg"
+              alt="Logo Image"
+              width={30}
+              height={30}
+              className="w-full"
+            />
+          </div>
+          <button
+            onClick={toggleMenu}
+            className="flex flex-col h-6 justify-between w-8 relative cursor-pointer"
+            aria-label="Toggle menu"
+          >
+            <motion.div
+              className="h-1 bg-black w-full origin-center"
+              animate={isMenuOpen ? { rotate: 45, y: 2 } : { rotate: 0, y: 0 }}
+              transition={{ duration: 0.3, ease: "easeInOut" }}
+            />
+            <motion.div
+              className="h-1 bg-black w-full"
+              animate={isMenuOpen ? { opacity: 0 } : { opacity: 1 }}
+              transition={{ duration: 0.3, ease: "easeInOut" }}
+            />
+            <motion.div
+              className="h-1 bg-black w-full origin-center"
+              animate={
+                isMenuOpen ? { rotate: -45, y: -19 } : { rotate: 0, y: 0 }
+              }
+              transition={{ duration: 0.3, ease: "easeInOut" }}
+            />
+          </button>
+        </div>
+
+        <motion.div
+          className="lg:hidden bg-white border-t border-gray-200 overflow-hidden shadow-lg rounded-md"
+          initial={false}
+          animate={
+            isMenuOpen
+              ? { height: "auto", opacity: 1 }
+              : { height: 0, opacity: 0 }
+          }
+          transition={{ duration: 0.3, ease: "easeInOut" }}
+        >
+          <div className="px-4 py-6 space-y-4">
+            <ul className="space-y-4 text-center *:cursor-pointer">
+              <li>
+                <Link
+                  href="/"
+                  className="block text-lg hover:text-blue-600 transition-colors"
+                >
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="/#"
+                  className="block text-lg hover:text-blue-600 transition-colors"
+                >
+                  About
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="/#"
+                  className="block text-lg hover:text-blue-600 transition-colors"
+                >
+                  Donate
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="/#"
+                  className="block text-lg hover:text-blue-600 transition-colors"
+                >
+                  Contact
+                </Link>
+              </li>
+            </ul>
+            <div className="pt-4 border-t border-gray-200 flex items-center gap-4 justify-center">
+              <Link href="https://www.youtube.com/@Djisrael27" target="_blank">
+                <FaYoutube size={35} />
+              </Link>
+
+              <Link
+                href="https://www.pinterest.com/akinpelu_israel/"
+                target="_blank"
+              >
+                <FaPinterestP size={35} />
+              </Link>
+            </div>
+          </div>
+        </motion.div>
+      </div>
     </nav>
   );
 }
